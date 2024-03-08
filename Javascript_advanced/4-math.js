4581231801a52de156717fd9ec085223d5c4b00a
mrktptn <marktipton95@gmail.com>
2024-03-08 15:48:17 -0600

    added closures

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -7,3 +7,8 @@ function addBy(firstNumber) {
     return (firstNumber + secondNumber);
   }
 }
+
+addBy100 = addBy(100);
+addBy1000 = addBy(1000);
+divideBy10 = divideBy(10);
+divideBy100 = divideBy(100);
