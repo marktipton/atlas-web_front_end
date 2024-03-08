2fb3cbb31320a3e802c7a3f1e8fa79936c7ef38e
mrktptn <marktipton95@gmail.com>
2024-03-08 16:16:23 -0600

    added return function fo r closure

---
+++ Javascript_advanced/5-mode.js
@@ -0,0 +1,17 @@
+function changeMode(size, weight, transform, background, color) {
+  return function () {
+    document.body.style.fontSize = size;
+    document.body.style.fontWeight = weight;
+    document.body.style.fontTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  }
+}
+
+
+
+function main() {
+  return function () {
+    document.body.style.fontSize = si
+  }
+}
