7098190ce650a42459420ffd8f15cb783b3a6533
mrktptn <marktipton95@gmail.com>
2024-03-08 18:50:39 -0600

    orders processed and logged in correct order

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -7,3 +7,9 @@ function processOrder (orderId, amount) {
   processPayment(amount);
   console.log(orderId + ' has been fully processed');
 }
+
+console.log('Processing orders');
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+console.log('All the orders have been processed');
