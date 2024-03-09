206eedcf9fd7ca06483e2b6d651b931bcd593b75
mrktptn <marktipton95@gmail.com>
2024-03-08 18:19:42 -0600

    changed loop to start at 1

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -4,7 +4,7 @@ setTimeout(function() {
   console.log('Final code block to be executed');
 }, 0);
 
-for (let i=0; i < 100; i++) {
+for (let i=1; i < 101; i++) {
   console.log(i);
 }
 console.log('End of the loop printing');
