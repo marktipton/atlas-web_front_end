a0d40a586ff3adf87d8635bf23a622d6ced4a3cd
mrktptn <marktipton95@gmail.com>
2024-03-11 16:48:40 -0500

    added user object and log function

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,13 @@
+const user = {
+  hobby: Calligraphy,
+  favoriteSport: Hockey,
+  astrologicalSign: Aries,
+  firstName: Buillaume,
+  lastName: Johns,
+  location: Netherlands,
+  occupation: Engineer
+};
+
+function logWelcomeUser(welcomeString) {
+  console.log(`${welcomeString}, ${firstName}. Your occupation is: ${occupation}`);
+}
