3025faef5d1138bccb0736853d3c64b2f1f1c16a
mrktptn <marktipton95@gmail.com>
2024-03-11 17:00:10 -0500

    fixed access to firstname and occupation. must access using user.firstname

--- Javascript_advanced/13-bind_user.js
+++ Javascript_advanced/13-bind_user.js
@@ -1,13 +1,17 @@
 const user = {
-  hobby: Calligraphy,
-  favoriteSport: Hockey,
-  astrologicalSign: Aries,
-  firstName: Buillaume,
-  lastName: Johns,
-  location: Netherlands,
-  occupation: Engineer
+  hobby: 'Calligraphy',
+  favoriteSport: 'Hockey',
+  astrologicalSign: 'Aries',
+  firstName: 'Buillaume',
+  lastName: 'Johns',
+  location: 'Netherlands',
+  occupation: 'Engineer'
 };
 
 function logWelcomeUser(welcomeString) {
-  console.log(`${welcomeString}, ${firstName}. Your occupation is: ${occupation}`);
+  console.log(`${welcomeString}, ${user.firstName}. Your occupation is: ${user.occupation}`);
 }
+
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+
+logWelcomeUser('Welcome');
