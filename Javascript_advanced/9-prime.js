43182c847a6647f9ef70df0500d9fe32b78a1c0c
mrktptn <marktipton95@gmail.com>
2024-03-11 15:52:09 -0500

    added basic setup for count prime numbers

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,8 @@
+function countPrimeNumbers() {
+  let i = 2;
+  for (; i < 101; i++) {
+
+  }
+}
+
+console.log(`Execution time of printing countPrimeNumbers was ${performance.now(countPrimeNumbers)} milliseconds.`);
