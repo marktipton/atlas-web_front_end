10215de47edbd9eb1ff9a1e3cfcaeb8f29bc97e2
mrktptn <marktipton95@gmail.com>
2024-03-11 16:12:30 -0500

    correct answer but too slow

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -1,8 +1,25 @@
 function countPrimeNumbers() {
+  const start = performance.now();
   let i = 2;
+  let count = 0;
   for (; i < 101; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
 
+    if (isPrime) {
+      count++;
+    }
   }
+  const end = performance.now();
+  const executionTime = end - start;
+  console.log(count);
+  console.log(`Execution time of printing countPrimeNumbers was ${performance.now(countPrimeNumbers)} milliseconds.`);
+  return count;
 }
 
-console.log(`Execution time of printing countPrimeNumbers was ${performance.now(countPrimeNumbers)} milliseconds.`);
+countPrimeNumbers();
