7b7ae7ee45f62b487d4cb34ca25a5ac5c0d746b1
mrktptn <marktipton95@gmail.com>
2024-03-12 15:52:19 -0500

    added xmlhttpsrequest to callback function

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -1,7 +1,13 @@
-function createElement (data) {
+const { default: BodyReadable } = require("undici-types/readable");
 
+function createElement (data) {
+  const paragraph = document.createElement('p');
+  paragraph.textContent = '';
+  document.body.append(paragraph);
 }
 
 function queryWikipedia (callback) {
-
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true);
 }
+
