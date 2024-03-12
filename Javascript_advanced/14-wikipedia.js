e2b49dcc80aa084f26b731b18ff674563327b26c
mrktptn <marktipton95@gmail.com>
2024-03-12 15:54:37 -0500

    checked xhr status and passed response data to callback function if succesful

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -9,5 +9,11 @@ function createElement (data) {
 function queryWikipedia (callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true);
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      const responseData = JSON.parse(xhr.responseText);
+      callback(null, responseData);
+    }
+  }
 }
 
