b8c7e8b940dbca77006e39faf2cf25a20bc34aa1
mrktptn <marktipton95@gmail.com>
2024-03-15 16:41:24 -0500

    edited show function to do dom

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -10,13 +10,16 @@ function showCookies() {
   var cookies = document.cookie.split(';');
 
   cookies.forEach(function(cookie) {
-    console.log(cookie.trim());
+    var paragraph = document.createElement('p');
+
+    paragraph.innerHTML = 'Cookies: ' + cookie.trim();
+    document.body.appendChild(paragraph);
   });
 }
 
 document.getElementById('logInButton').addEventListener('click', function() {
   setCookies();
-})
+});
 
 document.getElementById('showCookiesButton').addEventListener('click', function() {
   showCookies();
