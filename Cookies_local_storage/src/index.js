b30b47d9ecd0eb35b68460b60c5c8fd99a9f668d
mrktptn <marktipton95@gmail.com>
2024-03-15 16:26:53 -0500

    added showcookies function

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -3,5 +3,13 @@ function setCookies() {
 }
 
 function showCookies() {
+  var cookies = document.cookie.split(';');
 
+  cookies.forEach(function(cookie) {
+    console.log(cookie.trim());
+  });
 }
+
+document.getElementById('showCookiesButton').addEventListener('click', function() {
+  showCookies();
+});
